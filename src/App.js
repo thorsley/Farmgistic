5fc61f6d2933dcd93fc22907f91cc8798c40c827
Daniel Beaver <daniel.beaver1012@gmail.com>
2020-04-01 17:28:59 -0400

    added reactstrap import

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Splash from "./components/Splash/Splash";
+// import Auth from './Components/Auth/Auth';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Splash />
+        {/* <Auth />  */}
+      </div>
+    );
+  }
+}
+
+export default App;
