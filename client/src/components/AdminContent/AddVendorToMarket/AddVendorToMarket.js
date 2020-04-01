9821829c23fe4c4fcd941c58e693b634063dede3
AB <ab.schersch@gmail.com>
2020-04-01 17:05:12 -0400

    AB fetch CreateMarket

---
+++ client/src/components/AdminContent/AddVendorToMarket/AddVendorToMarket.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+class AddVendorToMarket extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleAdd = e => {
+        
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3003/booth/', {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTg1NzYzNDYxLCJleHAiOjE1ODU4NDk4NjF9.PCJPloDEWSsMahnTyw6Ip4U6Ewq8VNuly7SVvX1IEwY'
+                // 'Authorization': props.token
+            })
+        }).then ( (res) => res.json())
+        .then ( (data) => {
+            console.log(data);
+            
+        }).catch(error => console.error('Error:', error))
+    }
+    
+
+
+    render() {
+        return (
+            <div>
+                <p>Table of vendors with a button to add to Admin's Market</p>
+            </div>
+        )
+    }
+}
+
+export default AddVendorToMarket;
--- client/src/components/Market/CreateMarket/CreateMarket.js
+++ client/src/components/Market/CreateMarket/CreateMarket.js
@@ -10,45 +10,56 @@ class CreateMarket extends React.Component {
             size:""
         };
     }
-
-    shouldComponentUpdate() {
-        console.log('fetching..')
+    handleMarketName = e => {
+        this.setState({ marketName: e.target.value})
+    }
+    handleAddress = e => {
+        this.setState({address: e.target.value})
+    }
+    handleSize = e => {
+        this.setState({size: e.target.value})
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log("Market Name: ", this.state.marketName)
+        console.log("Address: ", this.state.address)
+        console.log("Size: ", this.state.size)
         fetch('http://localhost:3003/market/add/', {
             method: 'POST',
-            // body: JSON.stringify( {marketName: marketName, address: address, size: size}),
+            body: JSON.stringify( {marketName: this.state.marketName, address: this.state.address, size: this.state.size}),
             headers: new Headers({
                 'Content-Type': 'application/json',
-                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTg1NjcwNjI5LCJleHAiOjE1ODU3NTcwMjl9.Z5U0QP6lkRygszWo_kE-XwmWuocokt6I2cim5dKqc9k'
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTg1NzYzNDYxLCJleHAiOjE1ODU4NDk4NjF9.PCJPloDEWSsMahnTyw6Ip4U6Ewq8VNuly7SVvX1IEwY'
                 // 'Authorization': props.token
             })
         }).then ( (res) => res.json())
         .then ( (logData) => {
             console.log(logData);
             this.setState({
-                marketName: this.marketName,
-                address: this.address,
-                size: this.size
+                marketName: this.state.marketName,
+                address: this.state.address,
+                size: this.state.size
             })
-        })
+        }).catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response)); 
     }
     render() {
         return (
             <>
                 <h3>Create a Market</h3>
-                {/* <Form onSubmit={handleSubmit}> */}
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                 <FormGroup>
                         <Label htmlFor="marketName" />
-                        <Input name="marketName" value={this.marketName} onSubmit={(e) => this.setState(e.target.value)} />
+                        <Input name="marketName" type="text" value={this.marketName} onChange={this.handleMarketName} />
                     </FormGroup>
                     <FormGroup>
                         <Label htmlFor="address" />
-                        <Input name="address" value={this.address} onSubmit={(e) => this.setState(e.target.value)} >
+                        <Input type="text" name="address" value={this.address} onChange={this.handleAddress} >
                         </Input>
                     </FormGroup>
                     <FormGroup>
                         <Label htmlFor="size" />
-                        <Input name="size" value={this.size} onSubmit={(e) => this.setState(e.target.value)} />
+                        <Input type="text" name="size" value={this.size} onChange={this.handleSize} />
                     </FormGroup>
                     <Button type="submit">Click to Submit</Button>
                 </Form>
