1c6e28d9d23462d3c7a7117ed857fb8290ce0257
AB <ab.schersch@gmail.com>
2020-04-07 16:12:34 -0400

    styled navbar and headers

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -5,7 +5,6 @@ class Splash extends React.Component {
     render() {
         return(
             <div>
-                <h2 style={{color:"#7C8118"}}>FARM•GISTIC</h2>
                 {/* <p>Splash is sneaky and secretly holding everything displayed by the Navbar BrowserRouter</p> */}
                 <Farmbar />
             </div>
