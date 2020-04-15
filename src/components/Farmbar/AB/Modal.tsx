681a2c7151453664948cfdb68f8800c1def69cc8
AB <ab.schersch@gmail.com>
2020-04-15 09:17:44 -0400

    update TS on AB API

---
+++ src/components/Farmbar/AB/Modal.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Modal, Button } from 'antd';
+
+interface BoredState {
+  activity: string,
+  type: string,
+  accessibility: number,
+  isVisible: boolean,
+}
+
+
+type AcceptedProps = {
+}
+
+
+class ABModal extends React.Component<AcceptedProps, BoredState> {
+  constructor(props: AcceptedProps) {
+    super(props)
+    this.state = {
+        ...this.state,
+        isVisible: false,
+        activity: "",
+        type: "",
+        accessibility: 0,
+      }
+    }
+    componentWillMount() {
+    fetch('http://www.boredapi.com/api/activity/')
+    .then ( (res) => res.json())
+    .then ( json => {
+      this.setState({
+        activity: json.activity,
+        type: json.type,
+        accessibility: json.accessibility,
+      })
+      console.log(this.state.activity)
+    }).catch(error => console.error('Error:', error))
+}
+
+  showModal = (): boolean => {
+    this.setState({
+      isVisible: true,
+    });
+    return this.state.isVisible
+  };
+
+  handleOk = (): boolean => {
+    this.setState({
+      isVisible: false,
+    });
+    return this.state.isVisible
+  };
+
+  handleCancel = (): boolean => {
+    this.setState({
+      isVisible: false,
+    });
+    return this.state.isVisible
+  };
+  
+  render() {
+    let isVisible = false;
+    return (
+      <div>
+        <Button style={{border: "transparent"}} type="primary" onClick={this.showModal}>
+          AB
+        </Button>
+        <Modal
+          style={{fontWeight:'bold', color: '#C9E3EE', borderRadius: '3%', backgroundColor: '#656614', marginTop: '-50em',marginLeft: '30em',padding: '6em 9em'}}
+          title="Are you Bored?"
+          visible={this.state.isVisible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          cancelText={`    I'm not that bored    `}
+          okText={`Let's do ${this.state.type} stuff!`}
+          closable={false}
+        >
+          <br />
+          <p>Do you enjoy {this.state.type} activites?</p>
+          <br />
+          <p>How about you {this.state.activity.toLowerCase()}!</p>
+          <br />
+          <p>Would you agree that this activity is {100* (1- this.state.accessibility)}% accessible?</p>
+          <br />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+
+export default ABModal;
